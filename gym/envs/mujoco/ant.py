d67b693ba402c2855cf40beea7ab6e4e82d14d29
furuta <furuta0313@gmail.com>
2020-05-15 17:37:15 +0900

    fix ant

--- gym/envs/mujoco/ant.py
+++ gym/envs/mujoco/ant.py
@@ -2,37 +2,44 @@ import numpy as np
 from gym import utils
 from gym.envs.mujoco import mujoco_env
 
+
 class AntEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     def __init__(self):
         mujoco_env.MujocoEnv.__init__(self, 'ant.xml', 5)
         utils.EzPickle.__init__(self)
 
     def _step(self, a):
-        xposbefore = self.get_body_com("torso")[0]
-        self.do_simulation(a, self.frame_skip)
-        xposafter = self.get_body_com("torso")[0]
-        forward_reward = (xposafter - xposbefore)/self.dt
-        ctrl_cost = .5 * np.square(a).sum()
-        contact_cost = 0.5 * 1e-3 * np.sum(
-            np.square(np.clip(self.model.data.cfrc_ext, -1, 1)))
-        survive_reward = 1.0
+        self.do_simulation(action, self.frame_skip)
+        comvel = self.get_body_comvel("torso")
+        forward_reward = comvel[0]
+
+        if getattr(self, 'action_space', None):
+            action = np.clip(
+                action, self.action_space.low,
+                self.action_space.high
+            )
+            lb, ub = self.action_space.low, self.action_space.high
+        else:
+            lb, ub = -1, 1
+        scaling = (ub - lb) * 0.5
+        ctrl_cost = 0.5 * 1e-2 * np.sum(np.square(action / scaling))
+        contact_cost = 0.
+
+        survive_reward = 0.05
         reward = forward_reward - ctrl_cost - contact_cost + survive_reward
-        state = self.state_vector()
-        notdone = np.isfinite(state).all() \
-            and state[2] >= 0.2 and state[2] <= 1.0
-        done = not notdone
         ob = self._get_obs()
-        return ob, reward, done, dict(
-            reward_forward=forward_reward,
-            reward_ctrl=-ctrl_cost,
-            reward_contact=-contact_cost,
-            reward_survive=survive_reward)
+        notdone = np.isfinite(ob[:29]).all() and ob[2] >= 0.2 and ob[2] <= 1.0
+        done = not notdone
+        return ob, reward, done, {}
 
     def _get_obs(self):
         return np.concatenate([
-            self.model.data.qpos.flat[2:],
-            self.model.data.qvel.flat,
-            np.clip(self.model.data.cfrc_ext, -1, 1).flat,
+            self.model.data.qpos.flat,  # 15
+            self.model.data.qvel.flat,  # 14
+            # np.clip(self.model.data.cfrc_ext, -1, 1).flat,  # 84
+            self.get_body_xmat("torso").flat,  # 9
+            self.get_body_com("torso"),  # 3
+            self.get_body_comvel("torso"),  # 3
         ])
 
     def reset_model(self):
